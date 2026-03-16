514eb89d9fe31ca8ef11db2fea73b5ec9fc670c1
Luis fereira <fereiraluis18051997@gmail.com>
2026-03-16 12:36:49 -0300

    se modifico el middleware de las variables de entornos

--- src/app.js
+++ src/app.js
@@ -15,13 +15,6 @@ const swaggerDocs = (req, res, next) => {
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-// Middlewares
-
-console.log('Variables de entorno cargadas:');
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '✓ definida' : '✗ NO definida');
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('Tipo de DB_PASSWORD:', typeof process.env.DB_PASSWORD);
 
 // Usar rutas
 app.use('/openapi', swaggerUi.serve, swaggerDocs);
